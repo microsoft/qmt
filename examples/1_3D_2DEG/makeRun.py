27015189bc7214cc02b852f453330b1567b2a1ba
Kevin van Hoogdalem <kvanhoogdalem@gmail.com>
2018-05-31 12:52:27 +0200

    Working and documented 2DEG example

--- examples/1_3D_2DEG/makeRun.py
+++ examples/1_3D_2DEG/makeRun.py
@@ -10,20 +10,20 @@ import numpy as np
 import os
 
 # Path information -- change this to your configuration!
-rootPath = 'C:/Users/kevanhoo/OneDrive - Microsoft/Development/qmt/examples/1_3D_2DEG'
+rootPath = 'PATH_TO_QMT/qmt/examples/1_3D_2DEG'
 freeCADPath = os.path.join(rootPath, '2DEGFCDoc.FCStd')
 
 # These are necessary for running MS Proprietary components, but are not needed for building the model
-COMSOLExecPath = 'C:\\Program Files\\COMSOL\\COMSOL53a\\MultiphysicsROZ\\bin\\win64\\comsolclusterbatch'
-COMSOLCompilePath = 'C:\Program Files\COMSOL\COMSOL53a\MultiphysicsROZ\\bin\\win64\\comsolcompile'
+COMSOLExecPath = 'PATH_TO_COMSOL/Multiphysics/bin/win64/comsolclusterbatch'
+COMSOLCompilePath = 'PATH_TO_COMSOL/Multiphysics/bin/win64/comsolcompile'
 mpiPath = 'mpiexec'
-pythonPath = 'C:\\Users\\kevanhoo\\AppData\\Local\\Continuum\\Anaconda3\\envs\\qmt\\python'
-jdkPath = 'C:\\Program Files\\Java\\jdk1.8.0_144'
+pythonPath = 'PATH_TO_QMT_ENV/envs/qmt/python'
+jdkPath = 'PATH_TO_JDK/jdk1.8.0_144'
 
+numParallelJobs  = 2
 
 ####################
 ####################
-
 
 # Make the model file. This is a json file that handles all of the inter-module
 # communication and also serves as a record regarding what has been run.
@@ -32,49 +32,44 @@ runModel = qmt.Model(modelPath=modelPath)
 runModel.modelDict = runModel.genEmptyModelDict()
 
 # Import the materials we need:
-rootDir = qmt.__file__[:-12]
-matLib = qmt.Materials(matPath=rootDir + '/materials.json')
-matLib.load()
+matLib = qmt.Materials()
 runModel.modelDict['materials'] = matLib.serializeDict()
-
 
 # Tell FreeCAD what to do with the names we passed in. All the units here are in microns.
 # The simplest type of object directive is an extrude, which just takes one 2D
 # shape and makes it into a 3D prism.
-
-
 runModel.addPart('substrate','Rectangle','extrude','dielectric',
-                 material = 'In81Al19As',z0=(-0.5 - 0.004 - 0.005),
-                 thickness=0.5,meshMaxSize=0.2)
+                 material = 'GaAs',z0=(-0.5 - 0.004 - 0.005),
+                 thickness=0.5,meshMaxSize=0.1)
 runModel.addPart('backBarrier','Rectangle','extrude','dielectric',
-                 material = 'In81Ga19As',z0=(- 0.004 - 0.005),
-                 thickness=0.004,meshMaxSize=0.2)
+                 material = 'GaAs',z0=(- 0.004 - 0.005),
+                 thickness=0.004,meshMaxSize=0.02)
 runModel.addPart('quantumWell','Rectangle','extrude','semiconductor',
                  material = 'InAs',z0=(- 0.005),
-                 thickness=0.005,meshMaxSize=0.2)                
+                 thickness=0.005,meshMaxSize=0.02)                
 runModel.addPart('topBarrier','Rectangle','extrude','dielectric',
-                 material = 'In81Ga19As',z0=0.0,
-                 thickness=0.011,meshMaxSize=0.2)    
+                 material = 'GaAs',z0=0.0,
+                 thickness=0.011,meshMaxSize=0.02)   
 runModel.addPart('gateOxide','Rectangle','extrude','dielectric',
                  material = 'HfO2',z0=0.011,
-                 thickness=0.04,meshMaxSize=0.2)  
+                 thickness=0.04,meshMaxSize=0.02)  
 runModel.addPart('vacuum','Rectangle','extrude','dielectric',
                  material = 'air',z0=0.051,
                  thickness=0.5,meshMaxSize=0.2)                   
 
 runModel.addPart('AlFilm','i_AlEtch_Polyline003_sketch','extrude','metalGate',
-                 material = 'Al',z0=0.011,thickness=0.0087,meshMaxSize=0.2, boundaryCondition={'voltage' : 0.0})
+                 material = 'Al',z0=0.011,thickness=0.0087,meshMaxSize=0.05, boundaryCondition={'voltage' : 0.0})
 
 runModel.addPart('tunnelGate','i_TopGate1_Polyline007_sketch','extrude','metalGate',
-                 material = 'Au',z0=0.051,thickness=0.03,meshMaxSize=0.2, boundaryCondition={'voltage' : 0.0})
+                 material = 'Au',z0=0.051,thickness=0.03,meshMaxSize=0.05, boundaryCondition={'voltage' : 0.0})
 
 runModel.addPart('wireGate','i_TopGate1_Polyline008_sketch','extrude','metalGate',
-                 material = 'Au',z0=0.051,thickness=0.03,meshMaxSize=0.2, boundaryCondition={'voltage' : 0.0})
+                 material = 'Au',z0=0.051,thickness=0.03,meshMaxSize=0.05, boundaryCondition={'voltage' : 0.0})
 
 # To perform a voltage sweep, we assign it to one of our geometry objects like so:
 runModel.genPhysicsSweep('tunnelGate','V',np.linspace(0.0, -1.0, 3),unit='V') 
 
-# # Finalize the model:
+# Finalize the model:
 runModel.setPaths(COMSOLExecPath = COMSOLExecPath,\
                   COMSOLCompilePath=COMSOLCompilePath,\
                   mpiPath = mpiPath,\
@@ -82,10 +77,13 @@ runModel.setPaths(COMSOLExecPath = COMSOLExecPath,\
                   jdkPath=jdkPath,\
                   freeCADPath = freeCADPath)
 
-runModel.genComsolInfo(meshExport=None, fileName='comsolModel',physics=['electrostatics'],exportScalingVec=[0.5,5.,5.])
+runModel.genComsolInfo(meshExport=None, fileName='comsolModel',
+                        physics=['electrostatics'],
+                        exportScalingVec=[0.5,0.5,.5])
 
 runModel.addJob(rootPath, \
-                jobSequence=['geoGen', 'comsolRun'], numParallelJobs=numParallelJobs,
+                jobSequence=['geoGen', 'comsolRun'], 
+                numParallelJobs=numParallelJobs,
                 numCoresPerJob=4, comsolRunMode='debug')
 
 # Save and execute the model:
