f1e9b5f489f16827fc2c9b6201d598d1d89a9ec5
Bas Nijholt <basnijholt@gmail.com>
2018-07-10 21:53:46 -0700

    remove double imports and specify 'Harness' and 'Model'

--- qmt/__init__.py
+++ qmt/__init__.py
@@ -1,16 +1,12 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from .batchHarness import *
-from .batchModel import *
-
-try:  # This is to help if we need to import this from FreeCAD, which might lack sympy.
-    from .physics_constants import *
-except ImportError:
-    pass
-
+from .batchHarness import Harness
+from .batchModel import Model
 from .materials import *
+
+# This is to help if we need to import this from FreeCAD, which might lack sympy.
 try:
     from .physics_constants import *
-except BaseException:
+except ImportError:
     print('Could not import the units module, skipping.')
