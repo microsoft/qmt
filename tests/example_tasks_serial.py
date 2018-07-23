e07e4801c652e5926e9611f98b2d171fb773d34d
Donjan Rodic <donjan@dyx.ch>
2018-07-23 21:44:59 +0200

    moved non-unit tests to examples

---
+++ tests/example_tasks_serial.py
@@ -0,0 +1,14 @@
+from qmt.basic_tasks import GeometryTask, MaterialsTask
+from qmt.ms_tasks import PoissonTask
+
+geo_dict = {'part1':{'side length':5.},'part2':{'side length': 10.}}
+geo_task = GeometryTask(options=geo_dict)
+
+mat_dict = {'part1':{'material':'InAs'},'part2':{'material': 'InP'}}
+mat_task = MaterialsTask(geo_task,options=mat_dict)
+
+poisson_dict = {'part1':{'voltage':2.},'part2':{'voltage': 1.}}
+poisson_task = PoissonTask(geo_task,mat_task,options=poisson_dict)
+
+poisson_task.run()
+print poisson_task.result
