2c77895317567acbb3da5c5871fc3e298f45098d
Spencer Peters <t-sppete@microsoft.com>
2018-07-23 17:06:40 -0700

    removed __class__ in init

--- qmt/basic_tasks/geo_task.py
+++ qmt/basic_tasks/geo_task.py
@@ -3,7 +3,7 @@ from qmt.task_framework import Task
 class GeometryTask(Task):
 
     def __init__(self,options=None,name='geometry_task'):
-        super(self.__class__, self).__init__([], options, name)
+        super(GeometryTask, self).__init__([], options, name)
 
     def _solve_instance(self,input_result_list,current_options):
         return current_options
