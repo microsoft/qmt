3b48c2504b073414b283c04fdf39d28647b5dd67
Spencer Peters <t-sppete@microsoft.com>
2018-07-24 09:04:38 -0700

    finished first round comments

--- qmt/task_framework/task.py
+++ qmt/task_framework/task.py
@@ -18,6 +18,7 @@ Classes:
 from sweep import SweepHolder, gen_tag_extract, replace_tag_with_value
 from dask import delayed
 
+
 class TaskMetaclass(type):
     """
     Registers the class of each Task subclass for serialization.
@@ -35,6 +36,7 @@ class TaskMetaclass(type):
     @staticmethod
     def register_class(class_to_register):
         TaskMetaclass.class_registry[class_to_register.__name__] = class_to_register
+
 
 class Task(object):
     """
@@ -66,8 +68,6 @@ class Task(object):
         if "#" not in self.name:
             self.name += "#" + str(Task.current_instance_id)
             Task.current_instance_id += 1
-
-        # TODO we need to standardize the way that
 
         self.options = options
 
@@ -108,7 +108,8 @@ class Task(object):
         """
         Does the actual computation of this.
 
-        Note: Abstract method.
+        Note: Abstract method. Should be implemented by subclasses of Task.
+        Note: This is a callback and should not be called directly.
         :param input_result_list: The results produced by dependent tasks, in the order
         that the dependent tasks were given in the call to the Task base class constructor.
         :param current_options: The options of this corresponding to the current sweep iteration.
@@ -116,85 +117,105 @@ class Task(object):
         raise NotImplementedError("Task is missing the _solve_instance method!")
 
     def _populate_result(self):
+        """
+        Runs the sweep and populates self.result with dask.delayed objects.
+        """
         if self.previous_tasks is None:
             raise ValueError("A list of dependent tasks must be passed to the constructor by subclasses!")
         if self.sweep_manager is None:
+            # Treat the result as a single value to be computed
             input_result_list = [task.result for task in self.previous_tasks]
             self.result = delayed(self._solve_instance)(input_result_list, self.options)
         else:
+            # Make a SweepHolder to store results
             sweep_holder = SweepHolder(self.sweep_manager, self.list_of_tags)
             for sweep_holder_index, tag_values in enumerate(sweep_holder.tagged_value_list):
                 current_options = self._make_current_options(tag_values)
+
+                # Get the index in the total sweep
                 total_index = sweep_holder.index_in_sweep[sweep_holder_index][0]
+
+                # Use this index to get the appropriate results in dependent tasks
                 input_result_list = [task.result.get_object(total_index) for task in self.previous_tasks]
+
+                # Create a delayed object for this task's computation.
                 output = delayed(self._solve_instance)(input_result_list, current_options)
                 sweep_holder.add(output, sweep_holder_index)
             self.result = sweep_holder
 
-    def visualize(self, filename=None):
-        """
-        Visualizes the
-        :param filename:
-        :return:
-        """
-        return self.compile().visualize(filename=filename)
 
-    def run(self):
-        if self.result is None:
-            self.compile()
-        if self.sweep_manager is None:
-            self.result = self.result.compute()
-        else:
-            self.result.compute()
+def visualize(self, filename=None):
+    """
+    Visualizes the task graph of this.
 
-    # DEPRECATED serialization
-    # @staticmethod
-    # def from_dict(dict_representation):
-    #     taskName, data = dict_representation.items()[0]
-    #     className = data['class']
-    #     target_class = TaskMetaclass.class_registry[className]
-    #     kwargs = {}
-    #     for argName, argValue in data['argumentDictionary'].items():
-    #         if Task.isTaskRepresentation(argValue):
-    #             kwargs[argName] = Task.from_dict(argValue)
-    #         else:
-    #             kwargs[argName] = argValue
-    #     print(target_class)
-    #     return target_class(name=taskName, **kwargs)
-    #
-    # def save(self, file_name):
-    #     raise NotImplementedError("json serialization not yet implemented")
-    #     # TODO add serialization
+    :param filename: File to export the visualization to.
+    """
+    self.compile().visualize(filename=filename)
 
-        # def to_dict(self):
-        #     result = OrderedDict()
-        #     result_data = OrderedDict()
-        #     result_data['class'] = self.__class__.__name__
-        #     result_data['argumentDictionary'] = self.dependencies_dict()
-        #     result[self.name] = result_data
-        #     return result
 
-        # with open(file_name, 'w') as jsonFile:
-        #     json.dump(self.to_dict(), jsonFile)
-    #
-    # def dependencies_dict(self):
-    #     result = {}
-    #     for argName, argValue in self.argumentDictionary.items():
-    #         if isinstance(argValue, Task):
-    #             result[argName] = argValue.to_dict()
-    #         else:
-    #             result[argName] = argValue
-    #
-    #     return result
+def run(self):
+    """
+    Runs the task DAG graph whose root is this.
 
-    # @staticmethod
-    # def remove_self_argument(init_arguments):
-    #     init_arguments.pop('self', None)
-    #     return init_arguments
-    #
-    # @staticmethod
-    # def isTaskRepresentation(argValue):
-    #     # check if it has form {name:otherDict}
-    #     hasSingleItem = isinstance(argValue, dict) and len(argValue.items()) == 1
-    #     # check if it has the class to reconstitute the task from
-    #     return hasSingleItem and "class" in argValue.values()[0]
+    Replaces the delayed objects in this.result with actual results.
+
+    pre: this.compile() has been called to initialize the task graph.
+    """
+    if self.result is None:
+        self.compile()
+    if self.sweep_manager is None:
+        self.result = self.result.compute()
+    else:
+        self.result.compute()
+
+# DEPRECATED serialization
+# @staticmethod
+# def from_dict(dict_representation):
+#     taskName, data = dict_representation.items()[0]
+#     className = data['class']
+#     target_class = TaskMetaclass.class_registry[className]
+#     kwargs = {}
+#     for argName, argValue in data['argumentDictionary'].items():
+#         if Task.isTaskRepresentation(argValue):
+#             kwargs[argName] = Task.from_dict(argValue)
+#         else:
+#             kwargs[argName] = argValue
+#     print(target_class)
+#     return target_class(name=taskName, **kwargs)
+#
+# def save(self, file_name):
+#     raise NotImplementedError("json serialization not yet implemented")
+#     # TODO add serialization
+
+# def to_dict(self):
+#     result = OrderedDict()
+#     result_data = OrderedDict()
+#     result_data['class'] = self.__class__.__name__
+#     result_data['argumentDictionary'] = self.dependencies_dict()
+#     result[self.name] = result_data
+#     return result
+
+# with open(file_name, 'w') as jsonFile:
+#     json.dump(self.to_dict(), jsonFile)
+#
+# def dependencies_dict(self):
+#     result = {}
+#     for argName, argValue in self.argumentDictionary.items():
+#         if isinstance(argValue, Task):
+#             result[argName] = argValue.to_dict()
+#         else:
+#             result[argName] = argValue
+#
+#     return result
+
+# @staticmethod
+# def remove_self_argument(init_arguments):
+#     init_arguments.pop('self', None)
+#     return init_arguments
+#
+# @staticmethod
+# def isTaskRepresentation(argValue):
+#     # check if it has form {name:otherDict}
+#     hasSingleItem = isinstance(argValue, dict) and len(argValue.items()) == 1
+#     # check if it has the class to reconstitute the task from
+#     return hasSingleItem and "class" in argValue.values()[0]
