b1c339fe94eff4dd7885250f7d033ebc6855e208
John Gamble <johnkgamble@gmail.com>
2018-07-24 15:49:39 -0700

    Add stub for reduce_dim
    Also cleaned up geometry and test task.

--- qmt/basic_tasks/geometry.py
+++ qmt/basic_tasks/geometry.py
@@ -2,16 +2,19 @@
 # Licensed under the MIT License.
 
 from qmt.task_framework import Task
-from qmt.geometry import Part2D,Part3D
-class Geometry_1D(Task):
-    def __init__(self,options=None,name='geometry_1d_task'):
+from qmt.geometry import Part2D, Part3D
+
+
+class Geometry1D(Task):
+    def __init__(self, options=None, name='geometry_1d_task'):
         """
         Builds a geometry in 1D.
         :param options: The dictionary specifying parts, of the form {"part_name":(start_coord,stop_coord)}
         :param name: The name of this task.
         """
-        super(Geometry_1D, self).__init__([], options, name)
-    def _solve_instance(self,input_result_list,current_options):
+        super(Geometry1D, self).__init__([], options, name)
+
+    def _solve_instance(self, input_result_list, current_options):
         """
         Renders a geometry in 1D.
         :param input_result_list: This is an empty list.
@@ -20,17 +23,19 @@ class Geometry_1D(Task):
         """
         return current_options
 
-class Geometry_2D(Task):
-    def __init__(self,options=None,name='geometry_2d_task'):
+
+class Geometry2D(Task):
+    def __init__(self, options=None, name='geometry_2d_task'):
         """
         Builds a geometry in 2D.
         :param options: The dictionary specifying parts, of the form {"part_name":Part3D}
         :param name: The name of this task.
         """
-        super(Geometry_2D, self).__init__([], options, name)
+        super(Geometry2D, self).__init__([], options, name)
         for part_name in options:
             assert type(options[part_name]) is Part2D
-    def _solve_instance(self,input_result_list,current_options):
+
+    def _solve_instance(self, input_result_list, current_options):
         """
         Renders a geometry in 1D.
         :param input_result_list: This is an empty list.
@@ -39,8 +44,9 @@ class Geometry_2D(Task):
         """
         return current_options
 
-class Geometry_3D(Task):
-    def __init__(self,options=None,name='geometry_3d_task'):
+
+class Geometry3D(Task):
+    def __init__(self, options=None, name='geometry_3d_task'):
         """
         Builds a geometry in 3D.
         :param options: The dictionary specifying parts and and FreeCAD infromation. It should be of the form
@@ -48,12 +54,13 @@ class Geometry_3D(Task):
         {part_name:Part3D}.
         :param name: The name of this task.
         """
-        super(Geometry_3D, self).__init__([], options, name)
+        super(Geometry3D, self).__init__([], options, name)
         assert type(options["fcname"]) is str
         assert type(options["parts_dict"]) is dict
         for partDirective in options["part_dict"]:
-            assert type(options["part_dict"]) is Part3D
-    def _solve_instance(self,input_result_list,current_options):
+            assert type(options["part_dict"][partDirective]) is Part3D
+
+    def _solve_instance(self, input_result_list, current_options):
         """
         Renders a geometry in 1D.
         :param input_result_list: This is an empty list.
---
+++ qmt/basic_tasks/reduce_dim.py
@@ -0,0 +1,66 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+from qmt.task_framework import Task
+from qmt.basic_tasks import Geometry2D, Geometry3D
+
+
+class ReduceDim3D(Task):
+    def __init__(self, geo_3d, options=None, name='reduce_dim_3d_task'):
+        """
+        Reduces a 3D geometry to a 2D geometry for use with 2D solvers.
+        :param geo_3d: The input 3D geometry task
+        :param options: Dictionary of the form {"axis":tup,"position":pos,"parts":[part1,part2,...]}, where axis
+        is a 3-tuple defining the slice axis, position is the coordinate on the axis to take the cross-section, and
+        parts is an optional list of parts. If it is included, only the part names listed will be included.
+        :param name: The name of this task.
+        """
+        super(ReduceDim3D, self).__init__([geo_3d], options, name)
+        assert type(geo_3d) is Geometry3D
+        for spec in options: assert (spec == "axis") or (spec == "position") or (spec == "parts")
+        assert type(options["axis"]) is tuple
+        assert len(options["axis"]) == 3
+        assert type(options["position"]) is float
+        if "parts" in options:
+            for part in options["parts"]: assert type(part) is str
+
+    def _solve_instance(self, input_result_list, current_options):
+        """
+        Reduces a 3D geometry to a 2D geometry for use with 2D solvers.
+        :param input_result_list: This is a singleton list of a Geometry3D result.
+        :param current_options: The dictionary specifying parts from above.
+        :return geo_2d: A geometry2D object
+        """
+        geo_2d = current_options
+        return geo_2d
+
+
+class ReduceDim2D(Task):
+    def __init__(self, geo_2d, options=None, name='reduce_dim_2d_task'):
+        """
+        Reduces a 2D geometry to a 1D geometry for use with 1D solvers.
+        :param geo_2d: The input 2D geometry task
+        :param options: Dictionary of the form {"axis":tup,"position":pos,"parts":[part1,part2,...]}, where axis
+        is a 2-tuple defining the slice axis, position is the coordinate on the axis to take the cut, and
+        parts is an optional list of parts. If it is included, only the part names listed will be included.
+        :param name: The name of this task.
+        """
+        super(ReduceDim2D, self).__init__([geo_2d], options, name)
+        assert type(geo_2d) is Geometry2D
+        for spec in options: assert (spec == "axis") or (spec == "position") or (spec == "parts")
+        assert type(options["axis"]) is tuple
+        assert len(options["axis"]) == 2
+        assert type(options["position"]) is float
+        if "parts" in options:
+            for part in options["parts"]: assert type(part) is str
+
+    def _solve_instance(self, input_result_list, current_options):
+        """
+        Reduces a 2D geometry to a 1D geometry for use with 1D solvers.
+        :param input_result_list: This is a singleton list of a Geometry2D result.
+        :param current_options: The dictionary specifying parts from above.
+        :return geo_1d: A geometry1D object
+        """
+
+        geo_1d = current_options
+        return geo_1d
