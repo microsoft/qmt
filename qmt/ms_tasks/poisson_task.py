6adf8c12dec5a577d093cc358f0967e7b33eb405
Spencer Peters <t-sppete@microsoft.com>
2018-07-24 10:49:18 -0700

    removed more materials references

--- qmt/ms_tasks/poisson_task.py
+++ qmt/ms_tasks/poisson_task.py
@@ -9,12 +9,10 @@ class PoissonTask(Task):
         assert isinstance(geo_task, GeometryTask)
 
     def _solve_instance(self, input_result_list, current_options):
-        materials_result_instance = input_result_list[0]
-        geo_result_instance = input_result_list[1]
+        geo_result_instance = input_result_list[0]
         output = ''
         for part in geo_result_instance.keys():
             output += ' part: ' + part
             output += ', side length: ' + str(geo_result_instance[part]['side length'])
-            output += ', material: ' + materials_result_instance[part]['material']
             output += ', voltage: ' + str(current_options[part]['voltage'])
         return output
