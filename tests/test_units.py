48d0fb801f42fdf52ae79c4c208cb77c5c5fc659
Jan Gukelberger <jan.gukelberger@microsoft.com>
2018-07-24 14:03:54 -0700

    Clean up naming.

--- tests/test_units.py
+++ tests/test_units.py
@@ -18,13 +18,13 @@ def test_units():
 
 
 def test_constants():
-    assert pc.toFloat(c.m_e / u.g) == approx(9.1 * 1e-28, abs=1e-30)
-    assert pc.toFloat(
+    assert pc.to_float(c.m_e / u.g) == approx(9.1 * 1e-28, abs=1e-30)
+    assert pc.to_float(
         c.q_e / u.coulomb) == approx(1.60217662 * 1e-19, rel=1e-7)
-    assert pc.toFloat(c.mu_b / (u.eV / u.tesla)
-                      ) == approx(5.788382 * 1e-5, rel=1e-6)
-    assert pc.toFloat(c.q_e ** 2 / c.hbar / c.c / c.epsilon0 /
-                      4. / c.pi) == approx(1. / 137., abs=1e-4)
+    assert pc.to_float(c.mu_b / (u.eV / u.tesla)
+                       ) == approx(5.788382 * 1e-5, rel=1e-6)
+    assert pc.to_float(c.q_e ** 2 / c.hbar / c.c / c.epsilon0 /
+                       4. / c.pi) == approx(1. / 137., abs=1e-4)
 
 
 def test_matrices():
