f50aa60f9deb317c105b43e99542ac1382463788
Adam Frees <adamjfrees@gmail.com>
2018-07-26 22:54:37 +0000

    added ability to gather in tasks

--- qmt/task_framework/__init__.py
+++ qmt/task_framework/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from .sweep import SweepManager, SweepTag
+from .sweep import SweepManager, SweepTag, ReducedSweepFutures, ReducedSweepDelayed, ReducedSweep
 from .task import Task
