c7f85372ba0bf9e8dbacda86647ff9325873e0d2
Adam Frees <adamjfrees@gmail.com>
2018-08-06 18:34:23 +0000

    added build order to geo task, additions to 2d_SP example

--- examples/2d_SP.py
+++ examples/2d_SP.py
@@ -3,27 +3,31 @@
 
 import numpy as np
 import qmt.task_framework as qtf
-from shapely.geometry import Polygon
 import qmt.basic_tasks as qbt
-import qmt.ms_tasks as qmm
+import qms.tasks as qmst
 
 wireLength = qtf.SweepTag('wire length')
-#tag2 = qtf.SweepTag('v1')   
-
-#wireLength = 50.
+gateVoltage = qtf.SweepTag('gate voltage')
 
 geo_dict = {'parts' : {}, 'edges' : {}}
-geo_dict['parts']['wire'] = [(-wireLength/2., 0.), (0., wireLength/np.sqrt(2.)), (wireLength/2.,0.)]
+geo_dict['parts']['wire'] = [[-wireLength/2., 0.], [0., wireLength/np.sqrt(2.)], [wireLength/2.,0.]]
+geo_dict['edges']['gate'] = [[-wireLength/2., 0.], [0., wireLength/np.sqrt(2.)], [wireLength/2.,0.]]
 geo_task = qbt.Geometry2D(options=geo_dict)
 
-mesh_dict = {'mesh_type' : 'difference'}
+mesh_dict = {'mesh_type' : 'finite difference','grid' : {'x' : {'step_size' : 0.1}, 'y' : {'step_size' : 0.1}}}
 
-mesh_task = qmm.Mesh2D(geo_task, options = mesh_dict)
+mesh_task = qmst.Mesh2D(geo_task, options = mesh_dict)
 
-sweeps = [{wireLength : l} for l in np.linspace(50.,150.,3)]
+poisson_dict = {}
+poisson_dict['materials'] = {'wire' : 'InAs'}
+poisson_dict['voltages'] = {'gate' : gateVoltage}
+
+poisson_task = qmst.PoissonTask2D(geo_task, mesh_task, poisson_dict)
+
+sweeps = [{wireLength : l, gateVoltage : v} for l in np.linspace(50.,150.,3) for v in np.linspace(0.,1.,3)]
 
 sweep_man = qtf.SweepManager(sweeps)
 
-result = sweep_man.run(mesh_task)
-print(mesh_task.reduce())
+result = sweep_man.run(poisson_task)
+print(poisson_task.reduce())
 
