ade52b0b654e66bb5ea140d5acf2983fe3e1d5e4
EUROPE\kevanhoo <kevanhoo@microsoft.com>
2018-08-10 18:37:55 +0200

    adaptation to get_data for region_marker_file

--- qmt/geometry/geo_data.py
+++ qmt/geometry/geo_data.py
@@ -230,7 +230,9 @@ class Geo3DData(Data):
             data = fn.Mesh(tmp_path)
         else:
             import fenics as fn
-            data = fn.CellFunction(tmp_path)
+            mesh = self.get_data("mesh", scratch_dir='tmp_rmf_mesh')
+            data = fn.CellFunction('size_t', mesh)
+            fn.File(tmp_path) >> data
             # data = fn.MeshFunction(tmp_path)
         shutil.rmtree(scratch_dir)
         return data
