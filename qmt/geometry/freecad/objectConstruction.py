d100deffa8109b1da1f250de617105736d823206
Spencer Peters <t-sppete@microsoft.com>
2018-08-20 11:49:07 -0700

    debug

--- qmt/geometry/freecad/objectConstruction.py
+++ qmt/geometry/freecad/objectConstruction.py
@@ -91,7 +91,10 @@ def build(opts):
 def build_pass(part):
     '''Pas a part unchanged.'''
     assert part.directive == '3d_shape'
-    return FreeCAD.ActiveDocument.getObject(part.fc_name)
+    print >> sys.stderr, part.fc_name
+    result = FreeCAD.ActiveDocument.getObject(part.fc_name)
+    assert result is not None
+    return result
 
 
 def build_extrude(part):
