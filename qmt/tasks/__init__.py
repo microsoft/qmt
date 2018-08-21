95b948247c4c930541a1f2e2a17d9ccce3b4212d
Spencer Peters <t-sppete@microsoft.com>
2018-08-21 11:00:03 -0700

    big cleanup part n

--- qmt/tasks/__init__.py
+++ qmt/tasks/__init__.py
@@ -3,3 +3,4 @@
 from .task import Task
 from .sweep import ReducedSweep, ReducedSweepFutures, ReducedSweepDelayed, ReducedSweepResults, SweepManager, SweepTag
 from .basic.geometry import Geometry1D, Geometry2D, Geometry3D
+from .ms.mesh import Mesh, Mesh1D
