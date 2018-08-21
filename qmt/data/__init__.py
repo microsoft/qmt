1cf56fe20d217b243b4bf2d0a4897c641e5cbd86
Spencer Peters <t-sppete@microsoft.com>
2018-08-21 10:38:56 -0700

    big cleanup part n

--- qmt/data/__init__.py
+++ qmt/data/__init__.py
@@ -1,9 +1,2 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-
-from .density_data import Density1DData
-from .geo_data import Geo1DData, Geo2DData, Geo3DData
-from .part_data import Part3DData
-from .scalar_data import ScalarData
-# import frequently used names to qmt
-from .template import Data
