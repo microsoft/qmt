b80bf29262c96495842789424ac18816a148c5aa
Spencer Peters <t-sppete@microsoft.com>
2018-08-21 10:38:22 -0700

    big cleanup part n

--- qmt/tasks/__init__.py
+++ qmt/tasks/__init__.py
@@ -1,9 +1,2 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-
-from .basic import Geometry1D, Geometry2D, Geometry3D, ReduceDim2D, ReduceDim3D
-from .ms import Mesh, Mesh1D, Mesh2D, Mesh3D, PoissonTask
-from .sweep import SweepManager, SweepTag, ReducedSweep, ReducedSweepDelayed
-# import frequently used names to qmt
-from .task import Task
-
