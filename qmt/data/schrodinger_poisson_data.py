6e1e932ecca4acb75f938dd3963c489d38ff434d
Adam Frees <adamjfrees@gmail.com>
2018-08-29 20:19:15 +0000

    added to S-P data class.

--- qmt/data/schrodinger_poisson_data.py
+++ qmt/data/schrodinger_poisson_data.py
@@ -4,7 +4,7 @@
 from qmt.data import Data
 
 class SchrodingerPoissonData(Data):
-    def __init__(self, poisson_obj, density, density_units, rho, rho_units, mesh, mesh_units, bands, temperature, band_charges):
+    def __init__(self, poisson_obj, density, density_per_subband, density_units, rho, rho_units, psis, energies, potential, potential_units, mesh, mesh_units, bands, temperature, band_charges):
         """
         Constructs Data object stores outputs of Thomas-Fermi Task.
          
@@ -18,9 +18,14 @@ class SchrodingerPoissonData(Data):
 
         self.poisson = poisson_obj
         self.density = density
+        self.density_per_subband = density_per_subband
         self.density_units = density_units
         self.rho = rho
         self.rho_units = rho_units
+        self.psis = psis
+        self.energies = energies
+        self.potential = potential
+        self.potential_units = potential_units
         self.mesh = mesh
         self.mesh_units = mesh_units
         self.bands = bands
@@ -30,9 +35,14 @@ class SchrodingerPoissonData(Data):
     def _serialize(self):
         self.content['poisson'] = self.poisson
         self.content['density'] = self.density
+        self.content['density_per_subband'] = self.density_per_subband
         self.content['density_units'] = self._serialize_unit(self.density_units)
         self.content['rho'] = self.rho
         self.content['rho_units'] = self._serialize_unit(self.rho_units)
+        self.content['psis'] = self.psis
+        self.content['energies'] = self.energies
+        self.content['potential'] = self.potential
+        self.content['potential_units'] = self._serialize_unit(self.potential_units)
         self.content['mesh'] = self.mesh
         self.content['mesh_units'] = self._serialize_unit(self.mesh_units)
         self.content['bands'] = self.bands
@@ -42,9 +52,14 @@ class SchrodingerPoissonData(Data):
     def _deserialize(self):
         self.poisson = self.content['poisson']
         self.density = self.content['density']
+        self.density_per_subband = self.content['density_per_subband']
         self.denisty_units = self._deserialize_unit(self.content['density_units'])
         self.rho = self.content['rho']
         self.rho_units = self._deserialize_unit(self.content['rho_units'])
+        self.psis = self.content['psis']
+        self.energies = self.content['energies']
+        self.potential = self.content['potential']
+        self.potential_units = self._deserialize_unit(self.content['potential_units'])
         self.mesh = self.content['mesh']
         self.mesh_units = self._deserialize_unit(self.content['mesh_units'])
         self.bands = self.content['bands']
