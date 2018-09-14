fb592177816114c12b7bd2f793e70ad2e0473d6a
Adam Frees <adamjfrees@gmail.com>
2018-09-14 00:00:02 +0000

    added to data/_init

--- qmt/data/__init__.py
+++ qmt/data/__init__.py
@@ -6,3 +6,6 @@ from .density_data import Density1DData
 from .geo_data import Geo1DData, Geo2DData, Geo3DData
 from .part_data import Part3DData
 from .scalar_data import ScalarData3D, InterpolatableScalarData3D, FenicsPotentialData3D
+from .thomas_fermi_data import ThomasFermiData
+from .schrodinger_poisson_data import SchrodingerPoissonData
+from .mobility_data import MobilityData
