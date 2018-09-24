ff64b55c120b03d27b9ef716a7ada18361f5f4ca
Donjan Rodic <donjan@dyx.ch>
2018-09-24 14:32:56 +0200

    test_data_data_utils.py::test_store_serial cleanup

--- tests/py3/test_data_data_utils.py
+++ tests/py3/test_data_data_utils.py
@@ -16,7 +16,7 @@ def test_store_serial(fix_testDir, fix_FCDoc):
     serial_data = store_serial(fix_FCDoc, lambda d, p: d.saveAs(p), 'fcstd')
 
     # Write to a file
-    file_path = 'test.fcstd'
+    file_path = os.path.join(fix_testDir, 'test.fcstd')
     import codecs
     data = codecs.decode(serial_data.encode(), 'base64')
     with open(file_path, 'wb') as of:
@@ -28,3 +28,4 @@ def test_store_serial(fix_testDir, fix_FCDoc):
     doc.load(file_path)
 
     assert doc.getObject('some_content') is not None
+    os.remove(file_path)
