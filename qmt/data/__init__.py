433060232bca49ab09df02287f7fdce151ca1550
John Gamble <johnkgamble@gmail.com>
2018-09-25 19:56:40 -0700

    Delete geo1d

--- qmt/data/__init__.py
+++ qmt/data/__init__.py
@@ -3,7 +3,7 @@
 
 from .template import Data
 from .density_data import Density1DData
-from .geo_data import Geo1DData, Geo2DData, Geo3DData
+from .geo_data import Geo2DData, Geo3DData
 from .part_data import Part3DData
 from .thomas_fermi_data import ThomasFermiData
 from .schrodinger_poisson_data import SchrodingerPoissonData
--- qmt/data/geo_data.py
+++ qmt/data/geo_data.py
@@ -9,50 +9,6 @@ from qmt.data.template import Data
 from .data_utils import load_serial, store_serial, write_deserialised
 
 
-# TODO factor out geo superclass
-class GeoData(Data):
-    pass
-
-
-class Geo1DData(Data):
-    """Class holding 1D geometry data."""
-    def __init__(self):
-        """
-        Class for holding a 1D geometry specification.
-        """
-        super(Geo1DData, self).__init__()
-        self.parts = {}
-
-    def add_part(self, part_name, start_point, end_point, overwrite=False):
-        """
-        Add a part to this geometry.
-        :param str part_name: Name of the part to create
-        :param float start_point: Position of the start of the part
-        :param float end_point: Position of the end of the part
-        :param bool overwrite: Should we allow this to overwrite?
-        """
-        start, end = sorted([start_point, end_point])
-        if (part_name in self.parts) and (not overwrite):
-            raise ValueError("Attempted to overwrite the part " + part_name + ".")
-        else:
-            self.parts[part_name] = (start, end)
-
-    def remove_part(self, part_name, ignore_if_absent=False):
-        """
-        Remove a part from this geometry.
-        :param str part_name: Name of part to remove
-        :param bool ignore_if_absent: Should we ignore an attempted removal if the part name
-        is not found?
-        """
-        if part_name in self.parts:
-            del self.parts[part_name]
-        else:
-            if not ignore_if_absent:
-                raise ValueError(
-                    "Attempted to remove the part " + part_name + ", which doesn't exist.")
-            else:
-                pass
-
 
 class Geo2DData(Data):
     """Class holding 2D geometry data."""
@@ -130,6 +86,8 @@ class Geo2DData(Data):
                     "Attempted to remove the edge " + edge_name + ", which doesn't exist.")
             else:
                 pass
+
+
 
 
 class Geo3DData(Data):
