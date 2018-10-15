4d4eed0da57cff7ea5dbac97b4048402a027f0c5
John Gamble <johnkgamble@gmail.com>
2018-10-14 17:24:37 -0700

    Add data access routines to _init_

--- qmt/data/__init__.py
+++ qmt/data/__init__.py
@@ -1,7 +1,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from .data_utils import load_serial, store_serial, write_deserialised, serialised_file
+from .data_utils import load_serial, store_serial, write_deserialised, serialised_file,reduce_data,\
+                        retrieve_data, stream_data_to_file
 from .geometry import Geo2DData, Geo3DData, Part3DData
 from .solvers_2d import Potential2dData, ThomasFermi2dData,Bdg2dData
 from .solvers_3d import Fem3DData, serialize_fenics_function, deserialize_fenics_function, \
