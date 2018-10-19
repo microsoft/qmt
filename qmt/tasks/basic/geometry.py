44eba44a88106b638797971c4c1ef9e691aedee3
John Gamble <johnkgamble@gmail.com>
2018-10-19 09:52:17 -0700

    Revise 2D geometry task

--- qmt/tasks/basic/geometry.py
+++ qmt/tasks/basic/geometry.py
@@ -6,57 +6,39 @@
 from shapely.geometry import Polygon, LineString
 
 from qmt.data import Geo2DData, Geo3DData, serialised_file
-from qmt.tasks import Task
 
-class Geometry2D(Task):
-    """Task for handling of 2D geometries with Shapely."""
-    def __init__(self, options=None, name='geometry_2d_task'):
-        """
-        Builds a geometry in 2D.
-        :param dict options: The dictionary holding parts and edges. It should be of the form:
-        {
-        'parts':{'part_name':list of 2d points},
-        'edges':{'edge_name':list of 2d points},
-        'lunit':length_unit (nm),
-        'build_order':[list, of, all, parts]
-        }
-        , where these lists are turned into Polygon and
-        LineString objects, which are instances of shapely.geometry.
-        The build_order controls the order in which the objects are parsed, which matters for
-        overlapping domains and overlapping boundary conditions. The highest priority items are
-        listed first.
-        :param str name: The name of this task.
-        """
-        super(Geometry2D, self).__init__([], options, name)
-
-    @staticmethod
-    def _solve_instance(input_result_list, current_options):
-        """
-        :param list input_result_list: This is an empty list.
-        :param dict current_options: The dictionary specification from above.
-        :return: geo_2d: A Geo2DData object.
-        """
-        geo_2d = Geo2DData()
-        build_order = list(current_options.get('build_order',
-                                               current_options['parts']))
-        edges = current_options.get('edges',{})
-        # Set up the complete build order:
-        for part in current_options['parts']:
-            if part not in build_order:
-                build_order.append(part)
-        for edge in edges:
-            if edge not in build_order:
-                build_order.append(edge)
-        for object_name in build_order:
-            if object_name in current_options['parts']:
-                geo_2d.add_part(object_name, Polygon(current_options['parts'][object_name]))
-            elif object_name in edges:
-                geo_2d.add_edge(object_name, LineString(current_options['edges'][object_name]))
-            else:
-                raise ValueError("Object of name "+object_name+" was found neither in edges nor "
-                                                               "parts.")
-        geo_2d.lunit = current_options.get('lunit', 'nm')
-        return geo_2d
+def geometry_2d(parts,edges,lunit='nm',build_order=None):
+    """
+    Builds a geometry in 2D.
+    :param dict parts: Dictionary for holding the 2D parts, of the form
+    {'part_name':list of 2d points}.
+    :param dict edges: Dictionary of 2D edges, of the form:
+    {'edge_name':list of 2d points}.
+    :param str lunit: length_unit (nm).
+    :param list build_order: None or a list of all parts, determining the build order. Items on
+    the left are highest priority and items on the right are lowest. If None is given (default),
+    then build order is determined just taken to be the order of the parts and edges.
+    """
+    geo_2d = Geo2DData()
+    if build_order is None:
+        build_order = list(parts)
+    # Set up the complete build order:
+    for part in parts:
+        if part not in build_order:
+            build_order.append(part)
+    for edge in edges:
+        if edge not in build_order:
+            build_order.append(edge)
+    for object_name in build_order:
+        if object_name in parts:
+            geo_2d.add_part(object_name, Polygon(parts[object_name]))
+        elif object_name in edges:
+            geo_2d.add_edge(object_name, LineString(edges[object_name]))
+        else:
+            raise ValueError("Object of name "+object_name+" was found neither in edges nor "
+                                                           "parts.")
+    geo_2d.lunit = lunit
+    return geo_2d
 
 
 class Geometry3D(Task):
