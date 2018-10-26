1be8620768c73ff91633a8b10993f7e3b38b6076
John Gamble <johnkgamble@gmail.com>
2018-10-26 14:09:29 -0700

    Clean up old task framework

--- qmt/tasks/__init__.py
+++ qmt/tasks/__init__.py
@@ -1,5 +1,3 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-from .task import Task
-from .sweep import ReducedSweep, ReducedSweepFutures, ReducedSweepDelayed, ReducedSweepResults, SweepManager, SweepTag
-from .basic.geometry import geometry_2d, geometry_3d
+from .geometry import geometry_2d, geometry_3d
