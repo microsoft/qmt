4ff636f701776d30700a475c51f924d4e9abc5de
Georg Wolfgang Winkler <georg.wolfgang.winkler@gmail.com>
2019-01-09 12:05:00 -0800

    added phase_data to init

--- qmt/data/__init__.py
+++ qmt/data/__init__.py
@@ -5,6 +5,6 @@ from .data_utils import load_serial, store_serial, write_deserialised, serialize
     reduce_data, \
     retrieve_data, stream_data_to_file
 from .geometry import Geo2DData, Geo3DData, Part3DData
-from .solvers_2d import Potential2dData, ThomasFermi2dData, Bdg2dData
+from .solvers_2d import Potential2dData, ThomasFermi2dData, Bdg2dData, Phase2dData
 from .solvers_3d import Fem3DData, serialize_fenics_function, deserialize_fenics_function, \
     TransportData
