c1c44aed684ebbea1254500b47c4535f77062df4
Kewei Li <kewl@microsoft.com>
2019-03-28 16:57:40 -0700

    fix(geom): unnecessary import in test

--- tests/test_geo_task.py
+++ tests/test_geo_task.py
@@ -2,7 +2,6 @@
 # Licensed under the MIT License.
 
 """Testing the geometry task."""
-import FreeCAD
 
 def test_geo_task(datadir):
     """
@@ -41,7 +40,6 @@ def test_geo_task(datadir):
         built_geo = build_3d_geometry(input_parts=build_order,input_file=input_file_path,
                                       params={'d1': d1})
         results += [built_geo]
-    
 
     # Investigate results
     with tempfile.TemporaryDirectory() as temp_dir_path:
