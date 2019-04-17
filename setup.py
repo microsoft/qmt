f6fd03c6153adab8bea954c14b21f2fff61f5bbd
Bas Nijholt <basnijholt@gmail.com>
2019-04-17 15:52:21 -0700

    add miniver

--- setup.py
+++ setup.py
@@ -4,9 +4,24 @@
 
 from setuptools import setup
 
+
+# Loads _version.py module without importing the whole package.
+def get_version_and_cmdclass(package_name):
+    import os
+    from importlib.util import module_from_spec, spec_from_file_location
+
+    spec = spec_from_file_location("version", os.path.join(package_name, "_version.py"))
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module.__version__, module.cmdclass
+
+
+version, cmdclass = get_version_and_cmdclass("qmt")
+
 setup(
     name="qmt",
-    version="1.0",
+    version=version,
+    cmdclass=cmdclass,
     description="Qubit Modeling Tools (QMT) for computational modeling of quantum devices",
     url="https://github.com/Microsoft/qmt",
     author="Andrey Antipov, John Gamble, Jan Gukelberger, Donjan Rodic, Kevin van Hoogdalem, Georg Winkler",
