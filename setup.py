cb78bc2a0ae5a2814f0ca5022971d8b6c6e78baf
Bas Nijholt <basnijholt@gmail.com>
2019-04-18 14:26:01 -0700

    add miniver, get __version__ from git tags (#110)
    * add miniver
    
    * require Python >=3.6

--- setup.py
+++ setup.py
@@ -3,10 +3,32 @@
 # Licensed under the MIT License.
 
 from setuptools import setup
+import sys
+
+
+if sys.version_info < (3, 6):
+    print("qmt requires Python 3.6 or above.")
+    sys.exit(1)
+
+
+# Loads _version.py module without importing the whole package.
+def get_version_and_cmdclass(package_name):
+    import os
+    from importlib.util import module_from_spec, spec_from_file_location
+
+    spec = spec_from_file_location("version", os.path.join(package_name, "_version.py"))
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module.__version__, module.cmdclass
+
+
+version, cmdclass = get_version_and_cmdclass("qmt")
 
 setup(
     name="qmt",
-    version="1.0",
+    version=version,
+    python_requires=">=3.6",
+    cmdclass=cmdclass,
     description="Qubit Modeling Tools (QMT) for computational modeling of quantum devices",
     url="https://github.com/Microsoft/qmt",
     author="Andrey Antipov, John Gamble, Jan Gukelberger, Donjan Rodic, Kevin van Hoogdalem, Georg Winkler",
