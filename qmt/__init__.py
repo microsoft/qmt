655dc8965c3a3d175e72e3702380fa0aaf2c810d
Bas Nijholt <basnijholt@gmail.com>
2019-04-25 10:20:35 -0700

    make FreeCAD importable if installed via conda

--- qmt/__init__.py
+++ qmt/__init__.py
@@ -5,5 +5,13 @@ from ._version import __version__
 
 del _version
 
+import os
+import sys
+
+# make FreeCAD importable if installed via conda and when using an environment
+# remove when https://github.com/conda-forge/freecad-feedstock/issues/21) is fixed
+if "CONDA_PREFIX" in os.environ:
+    sys.path.append(os.path.join(os.environ["CONDA_PREFIX"], "lib"))
+
 from .physics_constants import units, constants, parse_unit, to_float
 from .materials import Materials
