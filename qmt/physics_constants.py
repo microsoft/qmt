7d280e55a9c842a948d106545a85ffc9d7603dde
Kewei Li <kewl@microsoft.com>
2019-05-21 07:00:38 +0000

    Uarray saveable

--- qmt/physics_constants.py
+++ qmt/physics_constants.py
@@ -8,6 +8,7 @@ from sympy.physics.matrices import msigma
 from sympy.physics.quantum import TensorProduct as kron
 from types import SimpleNamespace
 import numpy as np
+import deepdish
 
 
 units = SimpleNamespace(
@@ -156,11 +157,14 @@ matrices.tau_zy = kron(matrices.s_z, matrices.s_y)
 matrices.tau_zz = kron(matrices.s_z, matrices.s_z)
 
 
-class UArray(np.ndarray):
+class UArray(np.ndarray, deepdish.util.SaveableRegistry):
     """Extend a numpy array to have units information from sympy
     From https://docs.scipy.org/doc/numpy/user/basics.subclassing.html#simple-example-adding-an-extra-attribute-to-ndarray
 
     Pickle stuff copied from https://stackoverflow.com/questions/26598109/preserve-custom-attributes-when-pickling-subclass-of-numpy-array
+    
+    Deepdish save from https://deepdish.readthedocs.io/en/latest/io.html#class-instances
+    
     Parameters
     ----------
 
@@ -201,5 +205,12 @@ class UArray(np.ndarray):
         # Call the parent's __setstate__ with the other tuple elements.
         super().__setstate__(state[0:-1])
 
+    @classmethod
+    def load_from_dict(self, d):
+        obj = UArray(d["array"], d["unit"])
+        return obj
+
+    def save_to_dict(self):
+        return {"array": np.asarray(self), "unit": self.unit}
 
 __all__ = ["units", "constants", "matrices", "parse_unit", "to_float", "UArray"]
