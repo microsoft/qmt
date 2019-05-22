b9669cbbe9d4f8129a58c33001c80fe6e09accf3
Kewei Li <kewl@microsoft.com>
2019-05-22 09:49:20 +0000

    removing fenics

--- qmt/infrastructure/solvers_3d.py
+++ qmt/infrastructure/solvers_3d.py
@@ -1,15 +1,9 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from collections import namedtuple
-import sympy.physics.units as spu
-import kwant  # kwant import to stop fenics from segfaulting
-from qmt.infrastructure import store_serial, load_serial
-import fenics as fn
+import kwant
 from dataclasses import dataclass
-from typing import Dict, Optional
 from qmt.physics_constants import UArray
-from sympy.core.mul import Mul
 
 
 @dataclass
