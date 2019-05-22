3c952d1392d112f51a27026335d77821e72291e5
Kewei Li <kewl@microsoft.com>
2019-05-22 14:46:40 +0000

    kwant import

--- qmt/infrastructure/solvers_3d.py
+++ qmt/infrastructure/solvers_3d.py
@@ -1,9 +1,16 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import kwant
 from dataclasses import dataclass
 from qmt.physics_constants import UArray
+
+# fenics cannot be imported before kwant due to conflicting MUMPS loading mechanisms
+if "fenics" in dir():
+    del fenics
+    import kwant
+    import fenics
+else:
+    import kwant
 
 
 @dataclass
