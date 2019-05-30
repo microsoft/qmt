a9613ed7c267444e5f6e860b1330824e23a2ebc9
merrittlosert <merritt.p.losert.17@dartmouth.edu>
2019-05-30 15:55:43 -0500

    Update geometry sweep example (#144)

--- examples/geometry_sweep/geometry_sweep.py
+++ examples/geometry_sweep/geometry_sweep.py
@@ -6,67 +6,47 @@
 import os
 import numpy as np
 
-from qmt.data import Part3DData
-from qmt.tasks import build_3d_geometry
+from qmt.geometry import part_3d, build_3d_geometry
 
 # Set up geometry task
-block1 = Part3DData(
-    "Parametrised block",
-    "Sketch",
-    "extrude",
-    "dielectric",
-    material="air",
-    thickness=5.0,
-    z0=-2.5,
+
+block1 = part_3d.ExtrudePart("Parametrised block", "Sketch", thickness=5.0, z0=2.5)
+
+block2 = part_3d.ExtrudePart("Two blocks", "Sketch001", thickness=0.5)
+
+sag = part_3d.SAGPart(
+    "Garage", "Sketch002", thickness=6, z_middle=5, t_in=2.5, t_out=0.5, z0=0
 )
-block2 = Part3DData(
-    "Two blocks", "Sketch001", "extrude", "metal_gate", material="Au", thickness=0.5
-)
-sag = Part3DData(
-    "Garage",
-    "Sketch002",
-    "SAG",
-    "metal_gate",
-    material="Au",
-    z0=0,
-    z_middle=5,
-    thickness=6,
-    t_in=2.5,
-    t_out=0.5,
-)
-wire = Part3DData("Nanowire", "Sketch003", "wire", "semiconductor", z0=0, thickness=0.5)
-shell = Part3DData(
+
+wire = part_3d.WirePart("Nanowire", "Sketch003", z0=0, thickness=0.5)
+
+shell = part_3d.WireShellPart(
     "Wire cover",
     "Sketch004",
-    "wire_shell",
-    "metal_gate",
-    depo_mode="depo",
-    target_wire=wire,
     thickness=0.2,
+    target_wire=wire,
     shell_verts=[1, 2],
+    depo_mode="depo",
 )
-block3 = Part3DData("Passthrough", "Box", "3d_shape", "metal_gate")
-substrate = Part3DData(
-    "Substrate", "Sketch005", "extrude", "dielectric", z0=-2, thickness=2
-)
-wrap = Part3DData(
-    "First Layer",
+
+substrate = part_3d.ExtrudePart("Substrate", "Sketch005", thickness=2, z0=-2)
+
+wrap = part_3d.LithographyPart(
+    "First layer",
     "Sketch006",
-    "lithography",
-    "dielectric",
-    z0=0,
-    layer_num=1,
     thickness=0.4,
+    layer_num=1,
+    z0=0,
     litho_base=[substrate, wire, shell],
 )
-wrap2 = Part3DData(
-    "Second Layer", "Sketch007", "lithography", "dielectric", layer_num=2, thickness=0.1
-)
-virt = Part3DData("Virtual Domain", "Sketch008", "extrude", "virtual", thickness=5.5)
+
+wrap2 = part_3d.LithographyPart("Second Layer", "Sketch007", thickness=0.1, layer_num=2)
+
+virt = part_3d.ExtrudePart("Virtual Domain", "Sketch008", thickness=5.5, virtual=True)
 
 # Parameters for geometry building
 input_file = "geometry_sweep_showcase.fcstd"  # contains a model parameter 'd1'
-input_parts = [block1, block2, sag, virt, wire, shell, block3, substrate, wrap, wrap2]
+input_parts = [block1, block2, sag, virt, wire, shell, substrate, wrap, wrap2]
 
 # Compute parametrised geometries in parallel with dask
 geometries = []
