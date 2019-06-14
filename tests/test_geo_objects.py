0b6dead22e0484cc96ca91364370953be11b5313
Kewei Li <kewl@microsoft.com>
2019-06-14 23:15:42 +0000

    test crop

--- tests/test_geo_objects.py
+++ tests/test_geo_objects.py
@@ -1,8 +1,9 @@
-from qmt.geometry import part_3d, build_3d_geometry
+from qmt.geometry import part_3d, build_3d_geometry, Geo2DData
 import numpy as np
 import os
 import FreeCAD
 from qmt.geometry.freecad.geomUtils import checkOverlap
+from shapely.geometry import Polygon
 
 
 def test_xsection(datadir):
@@ -100,3 +101,51 @@ def test_overlapping_parts(datadir):
     shape_2 = doc.Objects[1]
     assert checkOverlap([shape_1, shape_2])
     FreeCAD.closeDocument(doc.Name)
+
+
+def test_crop():
+    r"""
+    The two shapes are:
+     ____________
+    |    ____    |
+    |   /    \   |
+    |  /      \  |
+    | /        \ |
+    |/__________\|
+    The inner rectangle is the crop
+     ____________
+    |  ________  |
+    | | /    \ | |
+    | |/      \| |
+    | /________\ |
+    |/__________\|
+    Which gives:
+     ________ 
+    | /    \ |
+    |/      \|
+    |________|
+    """
+    geo_data = Geo2DData()
+    outline = Polygon([[0, 0], [0, 5], [10, 5], [10, 0]])
+    inner = Polygon([[0, 0], [4, 4], [6, 4], [10, 0]])
+    outer = outline.difference(inner)
+    geo_data.add_part("inner", inner)
+    geo_data.add_part("outer", outer)
+    cropped_geo = geo_data.crop(x_min=1, x_max=9, y_min=1, y_max=4)
+    assert set(cropped_geo.parts["inner"].exterior.coords) == {
+        (1.0, 1.0),
+        (4.0, 4.0),
+        (6.0, 4.0),
+        (9.0, 1.0),
+    }
+    assert set(cropped_geo.parts["outer:0"].coords) == {(4.0, 4.0), (6.0, 4.0)}
+    assert set(cropped_geo.parts["outer:1"].exterior.coords) == {
+        (1.0, 1.0),
+        (1.0, 4.0),
+        (4.0, 4.0),
+    }
+    assert set(cropped_geo.parts["outer:2"].exterior.coords) == {
+        (6.0, 4.0),
+        (9.0, 1.0),
+        (9.0, 4.0),
+    }
